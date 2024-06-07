e264df4e62df40dcc5ed95dad24364e8400240ed
ariis11 <aristidaslukas@gmail.com>
2024-06-07 18:28:52 +0200

    secret split

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 const openai = new OpenAI({
-  apiKey: process.env.API_KEY
+  apiKey: process.env.API_KEY_1 + process.env.API_KEY_2
 });
 
 const aiService = {
