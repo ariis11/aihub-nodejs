724076244ce5a1835f5f76618beed6bc23c14502
ariis11 <aristidaslukas@gmail.com>
2024-06-07 22:52:58 +0200

    email sending

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ import express from 'express';
 import OpenAI from 'openai';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { google } from 'googleapis';
+import nodemailer from 'nodemailer';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const CLIENT_ID = '534696919943-hddf4qt3m7rnd2psmj01k1ps7skg9uon.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = '1//04iK6gHsueGZLCgYIARAAGAQSNwF-L9IrzaHfUsigFmyi5mEPRDTUulu8sFGkP5h8zMQF3X7dHfVK2-cgVSoSvXH8KCSOZ-gxKxU';
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 app.use(cors());
 
@@ -22,11 +32,57 @@ const aiService = {
     });
 
     return chatCompletion.choices[0].message;
-  }
+  },
+  // sendEmail: async () => {
+
+  // }
 };
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+});
+
+app.get('/sendEmail', async (req, res) => {
+  try {
+    const recipient = req.query.recipient;
+    const subject = req.query.subject;
+    const message = req.query.message;
+
+    if (recipient && subject && message) {
+      const accessToken = await oAuth2Client.getAccessToken();
+
+      const transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          type: 'OAuth2',
+          user: 'mariusrimkus97@gmail.com',
+          clientId: CLIENT_ID,
+          clientSecret: CLIENT_SECRET,
+          refreshToken: REFRESH_TOKEN,
+          accessToken: accessToken
+        },
+        tls: {
+          rejectUnauthorized: false
+        }
+      });
+
+      const mailOptions = {
+        from: 'Marius Rimkus <mariusrimkus97@gmail.com>',
+        to: recipient,
+        subject: subject,
+        text: message
+      };
+
+      const result = await transport.sendMail(mailOptions);
+
+      res.send(result);
+    } else {
+      res.status(400).send('Recipient, subject or message parameter is missing');
+    }
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("An error occurred while sending the email.");
+  }
 });
 
 app.get('/generateEmail', async (req, res) => {
